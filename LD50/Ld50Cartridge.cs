3b86a6ba611449270f65b2b39a0c2c4b994bd76b
notexplosive <thenotexplosive@gmail.com>
2022-04-01 17:24:48 -0700

    Solution is setup

---
+++ LD50/Ld50Cartridge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Machina.Engine;
+using Machina.Engine.Assets;
+using Machina.Engine.Cartridges;
+using Microsoft.Xna.Framework;
+
+namespace LD50
+{
+    class Ld50Cartridge : GameCartridge
+    {
+        public Ld50Cartridge() : base(new Point(1600, 900), ResizeBehavior.KeepAspectRatio)
+        {
+        }
+
+        public override void OnGameLoad(GameSpecification specification, MachinaRuntime runtime)
+        {
+        }
+
+        public override void PrepareDynamicAssets(AssetLoader loader, MachinaRuntime runtime)
+        {
+        }
+    }
+}
--- LD50/Program.cs
+++ LD50/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using Machina.Data;
+using Machina.Engine;
+using MachinaDesktop;
+using Microsoft.Xna.Framework;
 
 namespace LD50
 {
     public static class Program
     {
         [STAThread]
-        static void Main()
+        private static void Main(string[] args)
         {
-            using (var game = new Game1())
-                game.Run();
+            MachinaBootstrap.Run(
+                new GameSpecification("LD50 Game - NotExplosive.net", args, new GameSettings(new Point(1600, 900))),
+                new Ld50Cartridge(), ".");
         }
     }
 }
