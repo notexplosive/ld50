f4fae007cff70873c25febff238d88fd6d4c48d9
notexplosive <thenotexplosive@gmail.com>
2022-04-02 18:23:01 -0700

    Mana regeneration

--- LD50/Data/PartyMemberStatus.cs
+++ LD50/Data/PartyMemberStatus.cs
@@ -5,15 +5,19 @@ namespace LD50.Data
     public readonly struct PartyMemberStatus
     {
         private readonly int damageTaken;
+        private readonly float manaFraction;
         public BaseStats BaseStats { get; }
         public int Health => BaseStats.MaxHealth - this.damageTaken;
+        public int Mana { get; }
 
-        public PartyMemberStatus(BaseStats baseStats, Buffs appliedBuffs, int damageTaken = 0)
+        public PartyMemberStatus(BaseStats baseStats, Buffs appliedBuffs, int damageTaken = 0, int mana = 0, float manaFraction = 0f)
         {
             BaseStats = baseStats;
             this.damageTaken = damageTaken;
             Buffs = appliedBuffs;
             IsDead = damageTaken >= BaseStats.MaxHealth;
+            Mana = mana;
+            this.manaFraction = manaFraction;
         }
 
         public bool IsDead { get; }
@@ -28,7 +32,17 @@ namespace LD50.Data
             var newDamageTaken = this.damageTaken - healsThisFrame + damageThisFrame;
             newDamageTaken = Math.Clamp(newDamageTaken, 0, BaseStats.MaxHealth);
 
-            return new PartyMemberStatus(BaseStats, Buffs.GetUpdated(dt), newDamageTaken);
+            var exactNewMana = Mana + CalculateRegeneratedMana(dt) + this.manaFraction;
+            exactNewMana = Math.Clamp(exactNewMana, 0, BaseStats.MaxMana);
+            var roundedMana = (int) exactNewMana;
+            var newManaFraction = exactNewMana - roundedMana;
+
+            return new PartyMemberStatus(BaseStats, Buffs.GetUpdated(dt), newDamageTaken, roundedMana, newManaFraction);
+        }
+
+        private float CalculateRegeneratedMana(float dt)
+        {
+            return BaseStats.ManaRegenPerSecond * dt;
         }
 
         public Buffs GetBuffs()
---
+++ LD50Tests/ManaRegenerationTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LD50.Data;
+using Xunit;
+
+namespace LD50Tests
+{
+    public class ManaRegenerationTest
+    {
+        [Fact]
+        public void mana_regenerates_incrementally_small_numbers()
+        {
+            var status = new PartyMemberStatus(new BaseStats(100, 100, 1), new Buffs());
+            
+            var halfSecond = status.GetNext(0.5f, 0, 0);
+            var threeFourthsSecond = halfSecond.GetNext(0.25f, 0, 0);
+            var oneSecond = threeFourthsSecond.GetNext(0.25f, 0, 0);
+
+            halfSecond.Mana.Should().Be(0);
+            threeFourthsSecond.Mana.Should().Be(0);
+            oneSecond.Mana.Should().Be(1);
+        }
+    }
+}
