39d1a7baec1d59f1cb4d06602a1b5f858972d42b
notexplosive <thenotexplosive@gmail.com>
2022-04-02 17:05:42 -0700

    Add mana regen per second to base stats

--- LD50/Data/BaseStats.cs
+++ LD50/Data/BaseStats.cs
@@ -4,11 +4,13 @@
     {
         public int MaxHealth { get; }
         public int MaxMana { get; }
+        public int ManaRegenPerSecond { get; }
 
-        public BaseStats(int maxHealth, int maxMana)
+        public BaseStats(int maxHealth, int maxMana, int manaRegenPerSecond = 0)
         {
             MaxHealth = maxHealth;
             MaxMana = maxMana;
+            ManaRegenPerSecond = manaRegenPerSecond;
         }
     }
 }
